fb4a1dec9c6e25e3456cec54a8347d0ac9d6bd2a
San4illa <san4illa@bk.ru>
2021-08-14 13:00:07 +0300

    Extract location providers to app module

--- app/src/main/java/com/san4illa/weather/data/repository/location/GmsLocationDataSource.kt
+++ app/src/main/java/com/san4illa/weather/data/repository/location/GmsLocationDataSource.kt
@@ -1,19 +1,15 @@
 package com.san4illa.weather.data.repository.location
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.location.Location
-import com.google.android.gms.location.LocationServices
-import dagger.hilt.android.qualifiers.ApplicationContext
+import com.google.android.gms.location.FusedLocationProviderClient
 import javax.inject.Inject
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class GmsLocationDataSource @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val locationProvider: FusedLocationProviderClient
 ) {
-    private val locationProvider = LocationServices.getFusedLocationProviderClient(context)
-
     @SuppressLint("MissingPermission")
     suspend fun getLocation(): Location? {
         return suspendCoroutine { continuation ->
--- app/src/main/java/com/san4illa/weather/data/repository/location/HmsLocationDataSource.kt
+++ app/src/main/java/com/san4illa/weather/data/repository/location/HmsLocationDataSource.kt
@@ -1,18 +1,14 @@
 package com.san4illa.weather.data.repository.location
 
-import android.content.Context
 import android.location.Location
-import com.huawei.hms.location.LocationServices
-import dagger.hilt.android.qualifiers.ApplicationContext
+import com.huawei.hms.location.FusedLocationProviderClient
 import javax.inject.Inject
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class HmsLocationDataSource @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val locationProvider: FusedLocationProviderClient
 ) {
-    private val locationProvider = LocationServices.getFusedLocationProviderClient(context)
-
     suspend fun getLocation(): Location? {
         return suspendCoroutine { continuation ->
             locationProvider.lastLocation
