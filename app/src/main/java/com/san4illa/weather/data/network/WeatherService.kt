9451fed3f787932105e91cfba27c07c428c5d579
San4illa <san4illa@bk.ru>
2021-09-05 20:46:54 +0300

    Fix WeatherService

--- app/src/main/java/com/san4illa/weather/data/network/WeatherService.kt
+++ app/src/main/java/com/san4illa/weather/data/network/WeatherService.kt
@@ -3,11 +3,14 @@ package com.san4illa.weather.data.network
 import com.san4illa.weather.data.network.dto.WeatherDto
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface WeatherService {
-    @GET("{latitude},{longitude}?units=si&lang=ru")
+    @GET("{latitude},{longitude}")
     suspend fun getWeather(
         @Path("latitude") latitude: Double,
-        @Path("longitude") longitude: Double
+        @Path("longitude") longitude: Double,
+        @Query("units") units: String = "si",
+        @Query("lang") lang: String = "ru"
     ): WeatherDto
 }
