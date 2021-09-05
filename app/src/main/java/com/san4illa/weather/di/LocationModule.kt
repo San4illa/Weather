8669e562ef512c27793a8b1482a25f9fec1e7c8c
San4illa <san4illa@bk.ru>
2021-09-05 18:06:04 +0300

    Split AppModule to ApiModule and LocationModule

--- app/src/main/java/com/san4illa/weather/di/LocationModule.kt
+++ app/src/main/java/com/san4illa/weather/di/LocationModule.kt
@@ -1,37 +1,16 @@
 package com.san4illa.weather.di
 
 import android.content.Context
-import com.san4illa.weather.BuildConfig
-import com.san4illa.weather.data.network.WeatherService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
-    private const val BASE_URL = "https://api.darksky.net/forecast/${BuildConfig.API_KEY}/"
-
-    @Singleton
-    @Provides
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .addConverterFactory(MoshiConverterFactory.create())
-            .baseUrl(BASE_URL)
-            .build()
-    }
-
-    @Singleton
-    @Provides
-    fun provideApiService(retrofit: Retrofit): WeatherService {
-        return retrofit.create(WeatherService::class.java)
-    }
-
+object LocationModule {
     @Singleton
     @Provides
     fun provideGmsLocationProvider(
